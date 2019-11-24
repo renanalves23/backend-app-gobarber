1fa9eafff028c5d91f7b84da28ce400bd8320ec7
renan <renanalves23@outlook.com>
2019-11-24 19:36:13 -0300

    Commit de add routes e server

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
+//ESTRUTURA DA APLICAÇÃO separada do servidor
 const express = require('express');
+//importando rotas de outro arquivo
+const routes = require('./routes')
 
 class App {
-  constructor() }
+  constructor() {
     this.server = express();
 
-    app
+   //chamando as funções para dentro do constructor, senão nunca seriam chamadas 
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+      this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
 }
+
+//exportando uma nova instancia de App
+    //única coisa que deve ser acessada fora dessa classe é o server
+module.exports = new App().server;
